c0149fe3fab6ce449ed61e096141d7605ab87917
KonstantinIvanov <kokoivanov2008@gmail.com>
2025-10-05 11:25:00 +0300

    Compliting the sixth js exercise(repeatString).

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
+    let repeatStr = '';
+    if (times < 0) {
+        return 'ERROR';
+    }
+    
+    for (let i = 1; i <= times; i++) {
+       repeatStr += str;
+    }
 
+    return repeatStr;
 };
 
 // Do not edit below this line
