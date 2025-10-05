b70425e3a92406873e132bcb6dc3e2ebfed16189
KonstantinIvanov <kokoivanov2008@gmail.com>
2025-10-05 12:10:53 +0300

    Compliting the eight js exercise(removeFromArray.js).

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        if (args.includes(element)) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
