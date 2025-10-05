97c95a92b7e8f923cee4ab3f8429b47fa2af3d13
KonstantinIvanov <kokoivanov2008@gmail.com>
2025-10-05 11:42:30 +0300

    Compliting the seventh js exercise (reverseString.js).

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let array = str.split(' ');
+    let reverseArray = [];
+    for (const element of array) {
+        let newWord = '';
+        for (let i = element.length - 1; i >= 0; i--) {
+            const char = element[i];
+            newWord += char;
+        }
+        reverseArray.push(newWord);
+    }
+    str = reverseArray.reverse().join(' ');
 
+    return str;
 };
 
 // Do not edit below this line
