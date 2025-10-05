eb88e8b36334c5ecc6dbf9e0545327723ada9020
KonstantinIvanov <kokoivanov2008@gmail.com>
2025-10-05 12:52:26 +0300

    Compliting the eleventh js exercise(tempConversion.js).

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let result = ((fahrenheit - 32) * 5) / 9;
+  return result == 0 ? 0 : Number(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let result = celsius * 1.8 + 32;
+  return result == 0 ? 0 : Number(result.toFixed(1));
 };
 
 // Do not edit below this line
