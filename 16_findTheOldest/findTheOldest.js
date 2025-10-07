5e6e59f190a1316de7ac9b6ab822647439cb1969
KonstantinIvanov <kokoivanov2008@gmail.com>
2025-10-07 17:45:32 +0300

    Compliting th sixteenth js exercise(findTheOldest.js).

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people
+        .sort((a, b) => {
+            const getAge = person => {
+                const death = person.yearOfDeath ?? new Date().getFullYear();
+                return death - person.yearOfBirth;
+            }
 
+            return getAge(b) - getAge(a);
+        })
+        .at(0);
 };
 
 // Do not edit below this line
