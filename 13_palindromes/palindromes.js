60df358dc95d8f32c195d1dae4fbe6e2c07598a7
KonstantinIvanov <kokoivanov2008@gmail.com>
2025-10-07 16:52:34 +0300

    Compliting the thirtheen js exercise(palindromes.js).

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let isPalindromes = true;
+    str = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    let counter = str.length - 1;
+    for (let i = 0; i < str.length; i++) {
+        const forward = str[i];
+        
+        for (let j = counter; j > 0; j--) {
+            const backword = str[j];
+            if (forward === backword) {
+                counter--;
+                break;
+            }
 
+            isPalindromes = false;
+            return isPalindromes;
+        }
+    }
+
+    return isPalindromes;
 };
 
 // Do not edit below this line
